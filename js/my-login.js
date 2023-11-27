fca528888929b47aee09194dec2fa34e0bc57b3c
datlogarit <doandathchh@gmail.com>
2023-11-27 09:40:02 +0700

    add function login, register, change logo, pageNotFound

---
+++ js/my-login.js
@@ -0,0 +1,114 @@
+/******************************************
+ * My Login
+ *
+ * Bootstrap 4 Login Page
+ *
+ * 
+ * Help me to keep this project alive
+ * https://www.buymeacoffee.com/mhdnauvalazhar
+ * 
+ ******************************************/
+
+'use strict';
+function alert_notify(){
+	alert("Bạn đã tạo tài khoản thành công")
+}
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Nice, you triggered this alert message!', 'success')
+  })
+}	
+$(function () {
+
+	// author badge :)
+	// var author = '<div style="position: fixed;bottom: 0;right: 20px;background-color: #fff;box-shadow: 0 4px 8px rgba(0,0,0,.05);border-radius: 3px 3px 0 0;font-size: 12px;padding: 5px 10px;">By <a href="https://twitter.com/mhdnauvalazhar">@mhdnauvalazhar</a> &nbsp;&bull;&nbsp; <a href="https://www.buymeacoffee.com/mhdnauvalazhar">Buy me a Coffee</a></div>';
+	// $("body").append(author);
+
+	$("input[type='password'][data-eye]").each(function (i) {
+		var $this = $(this),
+			id = 'eye-password-' + i,
+			el = $('#' + id);
+
+		$this.wrap($("<div/>", {
+			style: 'position:relative',
+			id: id
+		}));
+
+		$this.css({
+			paddingRight: 60
+		});
+		$this.after($("<div/>", {
+			html: '<i class="fa-solid fa-eye"></i>',
+			class: 'show-password',
+			id: 'passeye-toggle-' + i,
+		}).css({
+			position: 'absolute',
+			right: 10,
+			top: ($this.outerHeight() / 2) - 12,
+			padding: '2px 7px',
+			cursor: 'pointer',
+		}));
+
+		$this.after($("<input/>", {
+			type: 'hidden',
+			id: 'passeye-' + i
+		}));
+
+		var invalid_feedback = $this.parent().parent().find('.invalid-feedback');
+
+		if (invalid_feedback.length) {
+			$this.after(invalid_feedback.clone());
+		}
+
+		$this.on("keyup paste", function () {
+			$("#passeye-" + i).val($(this).val());
+		});
+		$("#passeye-toggle-" + i).on("click", function () {
+			if ($this.hasClass("show")) {
+				$this.attr('type', 'password');
+				$this.removeClass("show");
+				$(this).removeClass("btn-outline-primary");
+			} else {
+				$this.attr('type', 'text');
+				$this.val($("#passeye-" + i).val());
+				$this.addClass("show");
+				$(this).addClass("btn-outline-primary");
+			}
+		});
+	});
+
+	$(".my-login-validation").submit(function () {
+		var form = $(this);
+		if (form[0].checkValidity() === false) {
+			event.preventDefault();
+			event.stopPropagation();
+		}
+		form.addClass('was-validated');
+	});
+});
+
+
+
+function onChange() {
+	const password = document.querySelector('input[name=password]');
+	const confirm = document.querySelector('input[name=confirm]');
+	if (confirm.value === password.value) {
+		confirm.setCustomValidity('');
+	} else {
+		confirm.setCustomValidity('Passwords do not match');
+	}
+}
